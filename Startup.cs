cd78a91c357079721953df8b30da1a1ce654f76a
xinglinp <72268684+xinglinp@users.noreply.github.com>
2020-12-14 14:19:05 +0800

    Client
    Bin is at Client2 folder

---
+++ Startup.cs
@@ -0,0 +1,89 @@
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(config =>{
+                //We check the cookie to confirm that we are authenticated
+                config.DefaultAuthenticateScheme = "ClientCookie";
+                //When we sign in we will deal out a cookie
+                config.DefaultSignInScheme = "ClientCookie";
+                //Use this to check if we are allowed to do something.
+                config.DefaultChallengeScheme = "OurServer";
+            })
+                .AddCookie("ClientCookie")
+                .AddOAuth("OurServier", config =>{
+                    config.ClientId = "client_id";
+                    config.ClientSecret = "client_secret";
+                    config.CallbackPath = "/oauth/callback";
+                    config. AuthorizationEndpoint = "https://localhost:44376/oauth/authorize";
+                    config.TokenEndpoint = "https://localhost:44376/oauth/token";
+
+                    config.SaveTokens = true;
+
+                    config.Events = new OAuthEvents()
+                    {
+                        OnCreatingTicket = context =>
+                        {
+                            var accessToken = context.AccessToken;
+                            var base64payload = accessToken.Split('.')[1];
+                            var bytes = Convert.FromBase64String(base64payload);
+                            var jsonPayload = Encoding.UTF8.GetString(bytes);
+                            var claims = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonPayload);
+                            
+                            foreach(var claim in claims)
+                            {
+                                context.Identity.AddClaim(new Claim(claim.Key, claim.Value));
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+
+            services.AddHttpClient();
+
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            // who are you?
+            app.UseAuthentication();
+
+            // are you allowed?
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //give me the default notation for controllers
+                endpoints.MapDefaultControllerRoute();
+
+            });
+        }
+    }
+}
